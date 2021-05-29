721874124e98a7583f0a9898e8badac8fa05a752
LAPTOP-747E5JBF\matep <matepranay11@gmail.com>
2021-05-29 22:21:45 +0530

    home-page

--- src/components/homePageSlides.js
+++ src/components/homePageSlides.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import {Carousel} from '3d-react-carousal';
+import '../css/homePageSlides.css';
 import '../css/homePageSlides.scss';
 import slides from './images';
 class homePageSlides extends Component {
